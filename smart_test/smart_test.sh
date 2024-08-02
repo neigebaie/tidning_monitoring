4a20bfb270f728640157d13fe586f54d6eab5cd0
neigebaie <paul.serander@gmail.com>
2024-08-03 00:33:53 +0200

    🐛 fix

--- smart_test/smart_test.sh
+++ smart_test/smart_test.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-# Run SMART test
-smartctl -H /dev/sda1 > /var/log/smart_test.log
+TEST_TYPE=$1
 
-# My HDD is /dev/sda1 but change it according to your setup
+# My HDD is $DRIVE but change it according to your setup
 # Use lsblk to check
+DRIVE=/dev/sda
+
+if [ "$TEST_TYPE" == "short" ]; then
+  # Start a short SMART test
+  smartctl -t short $DRIVE
+  # Sleep for one hour (300 seconds)
+  sleep 3600
+elif [ "$TEST_TYPE" == "long" ]; then
+  # Start a long SMART test
+  smartctl -t long $DRIVE
+  # Sleep for six hours (21600 seconds)
+  sleep 21600
+else
+  echo "Invalid test type. Use 'short' or 'long'."
+  exit 1
+fi
+
+# Check the self-test log and export to a log file
+smartctl -l selftest $DRIVE > /var/log/smart_selftest.log
+
+# Check the health status and export to a log file
+smartctl -H $DRIVE > /var/log/smart_health.log
