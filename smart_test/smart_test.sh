7a453ee6d21ef7063dbef6b3396160fb934be027
neigebaie <paul.serander@gmail.com>
2024-08-03 00:05:34 +0200

    🐛 fix

--- smart_test/smart_test.sh
+++ smart_test/smart_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run SMART test
-smartctl -H /hostfs/dev/sda1 > /var/log/smart_test.log
+smartctl -H /dev/sda1 > /var/log/smart_test.log
 
 # My HDD is /dev/sda1 but change it according to your setup
 # Use lsblk to check
